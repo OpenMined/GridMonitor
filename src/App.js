f1ed174ac643cf27fe96d4728d8cd51232f3254f
Héricles <hericles.me@gmail.com>
2020-04-11 02:19:33 -0300

    add header + image

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { ReactComponent as Logo } from './assets/img/gridwhite.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-
+      <header className="header">
+        <div className="brand">
+          <Logo className="logo" />
+          <h2 className="title"> Grid Monitor</h2>
+        </div>
+        <span>Address: http://35.178.211.39:8080/</span>
       </header>
     </div>
   );
