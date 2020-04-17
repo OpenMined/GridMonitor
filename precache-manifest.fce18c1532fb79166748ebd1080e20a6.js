dbc8e89cb274f102aa237144d1c479727779da9e
Héricles <hericles.me@gmail.com>
2020-04-17 20:03:12 -0300

    Updates

---
+++ precache-manifest.fce18c1532fb79166748ebd1080e20a6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6637182279f17dd11ac30c79fc663af2",
+    "url": "/GridMonitor/index.html"
+  },
+  {
+    "revision": "3c8f607570575480a053",
+    "url": "/GridMonitor/static/css/main.559b6391.chunk.css"
+  },
+  {
+    "revision": "2839a9fee9acf1989c78",
+    "url": "/GridMonitor/static/js/2.1479318b.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/GridMonitor/static/js/2.1479318b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c8f607570575480a053",
+    "url": "/GridMonitor/static/js/main.61edf52e.chunk.js"
+  },
+  {
+    "revision": "2c980f7aef9a6d632b08",
+    "url": "/GridMonitor/static/js/runtime-main.a46d5c82.js"
+  },
+  {
+    "revision": "bd2e1d32abef0b8f6cf58052a46209cc",
+    "url": "/GridMonitor/static/media/gradientbg.bd2e1d32.svg"
+  },
+  {
+    "revision": "189cbc262d8e0d04732c9a06adfbb14e",
+    "url": "/GridMonitor/static/media/gridwhite.189cbc26.svg"
+  },
+  {
+    "revision": "01b839c4b2342626b10e0f51b2b919f1",
+    "url": "/GridMonitor/static/media/pygrid.01b839c4.svg"
+  }
+]);
